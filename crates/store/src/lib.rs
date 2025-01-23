499727eb053ec9cc405571f3c4b292eb11acc3ac
Nikolay Chunosov <Chunosov.N@gmail.com>
2025-01-23 22:45:33 +0700

    disable birthday store
    Signed-off-by: Nikolay Chunosov <Chunosov.N@gmail.com>

--- crates/store/src/lib.rs
+++ crates/store/src/lib.rs
@@ -5,13 +5,14 @@ pub mod error;
 pub use error::Error;
 pub mod auth;
 pub mod calendar;
-mod contact_birthday_store;
+//mod contact_birthday_store;
 mod subscription_store;
 pub mod synctoken;
 
 pub use addressbook_store::AddressbookStore;
 pub use calendar_store::CalendarStore;
-pub use contact_birthday_store::ContactBirthdayStore;
+//pub use contact_birthday_store::ContactBirthdayStore;
+pub struct ContactBirthdayStore {}
 pub use subscription_store::*;
 
 pub use addressbook::{AddressObject, Addressbook};
