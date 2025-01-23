1de0fb483f67cc99b7a68424d78c1c189fc54f2a
Nikolay Chunosov <Chunosov.N@gmail.com>
2025-01-23 22:42:19 +0700

    add huly store, disable sqlite store
    Signed-off-by: Nikolay Chunosov <Chunosov.N@gmail.com>

---
+++ crates/store_huly/src/calendar_store.rs
@@ -0,0 +1,103 @@
+use async_trait::async_trait;
+use rustical_store::{Calendar, CalendarObject, CalendarStore, Error};
+use super::HulyStore;
+
+#[async_trait]
+impl CalendarStore for HulyStore {
+    async fn get_calendar(&self, principal: &str, id: &str) -> Result<Calendar, Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn get_calendars(&self, principal: &str) -> Result<Vec<Calendar>, Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn get_deleted_calendars(&self, principal: &str) -> Result<Vec<Calendar>, Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn update_calendar(
+        &self,
+        principal: String,
+        id: String,
+        calendar: Calendar,
+    ) -> Result<(), Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn insert_calendar(&self, calendar: Calendar) -> Result<(), Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn delete_calendar(
+        &self,
+        principal: &str,
+        name: &str,
+        use_trashbin: bool,
+    ) -> Result<(), Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn restore_calendar(&self, principal: &str, name: &str) -> Result<(), Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn sync_changes(
+        &self,
+        principal: &str,
+        cal_id: &str,
+        synctoken: i64,
+    ) -> Result<(Vec<CalendarObject>, Vec<String>, i64), Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn get_objects(
+        &self,
+        principal: &str,
+        cal_id: &str,
+    ) -> Result<Vec<CalendarObject>, Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn get_object(
+        &self,
+        principal: &str,
+        cal_id: &str,
+        object_id: &str,
+    ) -> Result<CalendarObject, Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn put_object(
+        &self,
+        principal: String,
+        cal_id: String,
+        object: CalendarObject,
+        overwrite: bool,
+    ) -> Result<(), Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn delete_object(
+        &self,
+        principal: &str,
+        cal_id: &str,
+        object_id: &str,
+        use_trashbin: bool,
+    ) -> Result<(), Error> {
+        Err(Error::NotFound)
+    }
+
+    async fn restore_object(
+        &self,
+        principal: &str,
+        cal_id: &str,
+        object_id: &str,
+    ) -> Result<(), Error> {
+        Err(Error::NotFound)
+    }
+
+    fn is_read_only(&self) -> bool {
+        false
+    }
+}
